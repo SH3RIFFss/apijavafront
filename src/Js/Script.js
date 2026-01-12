5d04913b14439574f919b31489d52bb35a1ee3ff
Felipe Barros <149625610+SH3RIFFss@users.noreply.github.com>
2026-01-11 21:51:11 -0400

    add metodo de reqisição

--- src/Js/Script.js
+++ src/Js/Script.js
@@ -1,58 +1,74 @@
-
+const apiUrl="https://apis3fjava.online/persistence/funcionario";
 function mostrarTabela(id) {
     const tabela = document.getElementById(id);
     tabela.classList.toggle("hidden");
 }
-
-function consumirFuncionario(id, nome, cargo) {
-    const funcionarios = document.getElementById("dadosFuncionarios");
-    const dadoFuncionario = document.createElement("tr");
-    dadoFuncionario.innerHTML = `<tr>
-    <td>${id}</td>
-    <td>${nome}</td>
-    <td>${cargo}</td>
-    </tr>`;
-    funcionarios.appendChild(dadoFuncionario);
+//teste de conexão com o back-end 
+async function conexao() {
+    try {
+        const resposta=await fetch(apiUrl,{
+            method:"GET",headers:{
+                "Content-type":"application/json"
+            }
+        });
+        if(!resposta.ok){
+            throw new Error(`erro na api....${resposta.status}`)
+        }
+        const dados=await resposta.json();
+        console.log("dados recebidos",dados);
+    } catch (error) {
+        console.log("fodeuuu.... ",error);
+    }
 }
-function consumirProduto(id, idFuncionario, quantidade, nome, valor, distribuidor, data) {
-    const produtos = document.getElementById("dadosProdutos");
-    const dadoProduto = document.createElement("tr");
-    dadoProduto.innerHTML = `<tr>
-                        <td>${id}</td>
-                        <td>${idFuncionario}</td>
-                        <td>${nome}</td>
-                        <td>${quantidade}</td>
-                        <td>${valor}</td>
-                        <td>${distribuidor}</td>
-                        <td>${data}</td>
-                    </tr>`;
-    produtos.appendChild(dadoProduto);
-}
-const persistirFuncionario = document.getElementById('btnFormFuncionario').addEventListener("click", (e) => {
-    e.preventDefault();
-    const nome = document.getElementById("nomeFuncionario");
-    const cargo = document.getElementById("cargoFuncionario");
-    const nomeV = nome.value;
-    const cargoV = cargo.value;
 
-    consumirFuncionario(0, nomeV, cargoV);
-})
-const persistirProduto = document.getElementById("btnFormProduto").addEventListener("click", (e) => {
-    e.preventDefault();
-    const idFuncionario = document.getElementById("idFuncionario");
-    const nome = document.getElementById("nome");
-    const valor = document.getElementById("valor");
-    const distribuidor = document.getElementById("distribuidor");
-    const data = document.getElementById("data");
-    const quantidade = document.getElementById("quantidade");
+// function consumirFuncionario(id, nome, cargo) {
+//     const funcionarios = document.getElementById("dadosFuncionarios");
+//     const dadoFuncionario = document.createElement("tr");
+//     dadoFuncionario.innerHTML = `<tr>
+//     <td>${id}</td>
+//     <td>${nome}</td>
+//     <td>${cargo}</td>
+//     </tr>`;
+//     funcionarios.appendChild(dadoFuncionario);
+// }
+// function consumirProduto(id, idFuncionario, quantidade, nome, valor, distribuidor, data) {
+//     const produtos = document.getElementById("dadosProdutos");
+//     const dadoProduto = document.createElement("tr");
+//     dadoProduto.innerHTML = `<tr>
+//                         <td>${id}</td>
+//                         <td>${idFuncionario}</td>
+//                         <td>${nome}</td>
+//                         <td>${quantidade}</td>
+//                         <td>${valor}</td>
+//                         <td>${distribuidor}</td>
+//                         <td>${data}</td>
+//                     </tr>`;
+//     produtos.appendChild(dadoProduto);
+// }
+// const persistirFuncionario = document.getElementById('btnFormFuncionario').addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const nome = document.getElementById("nomeFuncionario");
+//     const cargo = document.getElementById("cargoFuncionario");
+//     const nomeV = nome.value;
+//     const cargoV = cargo.value;
 
-    const idFuncionarioV = idFuncionario.value;
-    const nomeV = nome.value;
-    const valorV = valor.value;
-    const distribuidorV = distribuidor.value;
-    const dataV = data.value;
-    const quantidadeV = quantidade.value;
+//     consumirFuncionario(0, nomeV, cargoV);
+// })
+// const persistirProduto = document.getElementById("btnFormProduto").addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const idFuncionario = document.getElementById("idFuncionario");
+//     const nome = document.getElementById("nome");
+//     const valor = document.getElementById("valor");
+//     const distribuidor = document.getElementById("distribuidor");
+//     const data = document.getElementById("data");
+//     const quantidade = document.getElementById("quantidade");
 
-    consumirProduto(0, idFuncionarioV, quantidadeV, nomeV, valorV, distribuidorV, dataV);
-})
+//     const idFuncionarioV = idFuncionario.value;
+//     const nomeV = nome.value;
+//     const valorV = valor.value;
+//     const distribuidorV = distribuidor.value;
+//     const dataV = data.value;
+//     const quantidadeV = quantidade.value;
 
+//     consumirProduto(0, idFuncionarioV, quantidadeV, nomeV, valorV, distribuidorV, dataV);
+// })
